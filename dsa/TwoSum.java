fa46ae964e761a40c18ee1de2bfd85fe93a071f6
Divakaran <divakaran@solinas.in>
2025-10-22 16:50:17 +0530

    init

--- dsa/TwoSum.java
+++ dsa/TwoSum.java
@@ -1,16 +1,15 @@
 package dsa;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
 
     public static void main(String[] args) {
-        twoSum(new int[]{3,6,11,7}, 9);
+        twoSum(new int[]{3,11,6,7}, 9);
     }
 
     public static void twoSum(int[] nums, int target) {
-        int lengthOfTheArray = nums.length;
-
         // Brute Force Approach
 //        for(int i=0; i<lengthOfTheArray; i++){
 //            for(int j=i+1; j<lengthOfTheArray; j++){
@@ -22,9 +21,9 @@ public class TwoSum {
 //        }
 
         // Optimal Approach using HashMap
-        Map<Integer, Integer> map = new java.util.HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0; i<lengthOfTheArray; i++) {
+        for(int i=0; i< nums.length; i++) {
             int complement = target - nums[i];
             if(map.containsKey(complement)) {
                 System.out.println("Indices are: " + map.get(complement) + " " + i);
